98c7cbda7231ac637d8af1f62a69967dab3ae3b4
Vanessa Ferreira <vanessa.ferreira.dev@gmail.com>
2023-01-13 11:20:17 +0000

    Login and Register drafts

---
+++ src/containers/Login/Login.jsx
@@ -0,0 +1,81 @@
+import React, { useContext, useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { AuthContext } from "./containers/providers/userAuth"
+import { loginUser } from "../../services/apiCalls"
+import "./Login.css"
+// fazer alterações
+
+const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  })
+  const [error, setError] = useState("")
+  const navigate = useNavigate()
+
+  const { addUserToContext } = useContext(AuthContext)
+  const { addAdminToContext } = useContext(AuthContext)
+
+  const handleData = (e) => {
+    setUser((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const login = async (user) => {
+    let response = await loginUser(user)
+    if (response.length === 0 || response === false) {
+      setError("E-mail or password doesn't match")
+    } else {
+      addUserToContext(response[0])
+      setError("no error")
+      if (response[0].isAdmin === "true") {
+        addAdminToContext(response[0])
+        navigate("/admin")
+      } else navigate("/profile")
+    }
+  }
+
+  return (
+    <div className="loginDesign">
+      <input
+        className="myInput"
+        type="email"
+        name="email"
+        id="email"
+        title="email"
+        placeholder="E-mail:"
+        autoComplete="off"
+        onChange={(e) => {
+          handleData(e)
+        }}
+      />
+      <input
+        className="myInput"
+        type="password"
+        name="password"
+        id="password"
+        title="password"
+        placeholder="Password:"
+        autoComplete="off"
+        onChange={(e) => {
+          handleData(e)
+        }}
+      />
+      <div className="bottomSection">
+        <div
+          className="designLoginButton"
+          onClick={() => {
+            login(user)
+          }}
+        >
+          Login
+        </div>
+      </div>
+      <div>{error === "no error" ? null : error}</div>
+    </div>
+  )
+}
+
+export default Login
--- src/containers/Register/Register.jsx
+++ src/containers/Register/Register.jsx
@@ -1,23 +1,140 @@
 import React, { useState } from "react"
-import { errorValidate } from "../../services/validate"
 import "./Register.css"
+import { registerNewUser } from "../../services/apiCalls"
+import { validateForm } from "../../services/validate"
+import { checkEmail } from "../../services/apiCalls"
+import { useNavigate } from "react-router-dom"
 
 const Register = () => {
   const [user, setUser] = useState({
     name: "",
     email: "",
     phone: "",
+    adress: "",
     password: "",
-    password2: "",
+    isAdmin: "false",
+  })
+  const [error, setError] = useState("")
+  const navigate = useNavigate()
+
+  const handleData = (e) => {
+    setUser((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const register = async (user) => {
+    let validationError
+    if ((await checkEmail(user.email)) === false) {
+      validationError = "This E-mail is already registered"
+    } else {
+      validationError = validateForm(user)
+    }
+    setError(validationError)
+    if (error === "no error") {
+      registerNewUser(user)
+      navigate("/login")
+    }
+  }
+
+  return (
+    <div className="registerDesign">
+      <input
+        className="myInput"
+        type="text"
+        name="name"
+        id="name"
+        title="name"
+        placeholder="Name:"
+        autoComplete="off"
+        onChange={(e) => {
+          handleData(e)
+        }}
+      />
+
+      <input
+        className="myInput"
+        type="email"
+        name="email"
+        id="email"
+        title="email"
+        placeholder="E-mail:"
+        autoComplete="off"
+        onChange={(e) => {
+          handleData(e)
+        }}
+      />
+      <input
+        className="myInput"
+        type="phone"
+        name="phone"
+        id="phone"
+        title="phone"
+        placeholder="Contact:"
+        autoComplete="off"
+        onChange={(e) => {
+          handleData(e)
+        }}
+      />
+      <input
+        className="myInput"
+        type="address"
+        name="address"
+        id="address"
+        title="address"
+        placeholder="Address:"
+        autoComplete="off"
+        onChange={(e) => {
+          handleData(e)
+        }}
+      />
+      <input
+        className="myInput"
+        type="password"
+        name="password"
+        id="password"
+        title="password"
+        placeholder="Password:"
+        autoComplete="off"
+        onChange={(e) => {
+          handleData(e)
+        }}
+      />
+      <div className="bottomSection">
+        <div
+          className="designRegisterButton"
+          onClick={() => {
+            register(user)
+          }}
+        >
+          Register
+        </div>
+      </div>
+      <div>{error === "no error" ? null : error}</div>
+    </div>
+  )
+}
+
+export default Register
+
+/*const Register = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+    isAdmin: "false",
   })
 
   const [userError, setUserError] = useState({
-    nameError: "",
+    const navigate = useNavigate()
+    /*nameError: "",
     emailError: "",
     phoneError: "",
     passwordError: "",
     password2Error: "",
-  })
+  
 
   const handleChange = (event) => {
     const { name, value } = event.target
@@ -50,75 +167,56 @@ const Register = () => {
 
   return (
     <div className="registerPage">
-      <h1>Register</h1>
-
-      <div className="logoRegister">
-        {/* <img className ="logoDesign" src={} alt={} /> */}
-      </div>
-
-      <div className="registerInputs">
-        <input
-          className={
-            userError.nameError === "" ? "inputNormal" : "inputNormal inputError"
-          }
-          placeholder="Full name"
-          type="text"
-          name="name"
-          onChange={handleChange}
-          onBlur={errorHandler}
-        />
-        <div className="errorMessage"> {userError.nameError}</div>
-        <input
-          className={
-            userError.emailError === "" ? "inputNormal" : "inputNormal inputError"
-          }
-          placeholder="Email"
-          type="email"
-          name="email"
-          onChange={handleChange}
-          onBlur={errorHandler}
-        />
-        <div className="errorMessage"> {userError.emailError}</div>
-        <input
-          className={
-            userError.phoneError === "" ? "inputNormal" : "inputNormal inputError"
-          }
-          placeholder="Phone number"
-          type="text"
-          name="phone"
-          onChange={handleChange}
-          onBlur={errorHandler}
-        />
-        <div className="errorMessage"> {userError.phoneError}</div>
-        <input
-          className={
-            userError.passwordError === "" ? "inputNormal" : "inputNormal inputError"
-          }
-          placeholder="Password"
-          type="password"
-          name="password"
-          onChange={handleChange}
-          onBlur={errorHandler}
-        />
-        <div className="errorMessage"> {userError.passwordError}</div>
-        <input
-          className={
-            userError.password2Error === ""
+      <><h1>Register</h1><div className="logoRegister">
+        { <img className ="logoDesign" src={} alt={} /> }
+      </div><div className="registerInputs">
+          <input
+            className={userError.nameError === "" ? "inputNormal" : "inputNormal inputError"}
+            placeholder="Full name"
+            type="text"
+            name="name"
+            onChange={handleChange}
+            onBlur={errorHandler} />
+          <div className="errorMessage"> {userError.nameError}</div>
+          <input
+            className={userError.emailError === "" ? "inputNormal" : "inputNormal inputError"}
+            placeholder="Email"
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={errorHandler} />
+          <div className="errorMessage"> {userError.emailError}</div>
+          <input
+            className={userError.phoneError === "" ? "inputNormal" : "inputNormal inputError"}
+            placeholder="Phone number"
+            type="text"
+            name="phone"
+            onChange={handleChange}
+            onBlur={errorHandler} />
+          <div className="errorMessage"> {userError.phoneError}</div>
+          <input
+            className={userError.passwordError === "" ? "inputNormal" : "inputNormal inputError"}
+            placeholder="Password"
+            type="password"
+            name="password"
+            onChange={handleChange}
+            onBlur={errorHandler} />
+          <div className="errorMessage"> {userError.passwordError}</div>
+          <input
+            className={userError.password2Error === ""
               ? "inputNormal"
-              : "inputNormal inputError"
-          }
-          placeholder="Repeat password"
-          type="password"
-          name="password2"
-          onChange={handleChange}
-          onBlur={errorHandler}
-        />
-        <div className="errorMessage"> {userError.password2Error}</div>
+              : "inputNormal inputError"}
+            placeholder="Repeat password"
+            type="password"
+            name="password2"
+            onChange={handleChange}
+            onBlur={errorHandler} />
+          <div className="errorMessage"> {userError.password2Error}</div>
 
-        <div className="registerButton">Register</div>
-      </div>
+          <div className="registerButton">Register</div>
+        </div></>
     </div>
   )
 }
 
-export default Register
+export default Register*/

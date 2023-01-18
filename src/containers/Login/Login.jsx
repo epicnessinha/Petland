8992bb8070a23ee5b2d9dc8d40a72ce66694c05d
Vanessa Ferreira <vanessa.ferreira.dev@gmail.com>
2023-01-18 08:22:18 +0000

    Updates LoginContext and apiCalls

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,96 +1,52 @@
-import "./Login.css"
-const Home = () => {
-  return (
-    <div className="welcomeDesign">
-      <div className="back"></div>
-      <div className="Text-Wrapper">
-        <div className="left-text">
-          <h1 className="user">User Login</h1>
-          <h4 className="description1">Logic not done yet</h4>
-        </div>
-        <div className="right-text">
-          <h3></h3>
-        </div>
-      </div>
-    </div>
-  )
-}
+import React, { useState } from "react"
+import { useHistory } from "react-router-dom"
 
-export default Home
+function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const history = useHistory()
 
-// fazer alterações
-
-/*const Login = () => {
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-  })
-  const [error, setError] = useState("")
-  const navigate = useNavigate()
-
-  const { addUserToContext } = useContext(AuthContext)
-  const { addAdminToContext } = useContext(AuthContext)
-
-  const handleData = (e) => {
-    setUser((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }))
-  }
-
-  const login = async (user) => {
-    let response = await loginUser(user)
-    if (response.length === 0 || response === false) {
-      setError("E-mail or password doesn't match")
-    } else {
-      addUserToContext(response[0])
-      setError("no error")
-      if (response[0].isAdmin === "true") {
-        addAdminToContext(response[0])
-        navigate("/admin")
-      } else navigate("/profile")
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      })
+      const data = await response.json()
+      if (data.token) {
+        localStorage.setItem("token", data.token)
+        history.push("/dashboard")
+      } else {
+        console.error(data.message)
+      }
+    } catch (err) {
+      console.error(err)
     }
   }
 
   return (
-    <div className="loginDesign">
-      <input
-        className="myInput"
-        type="email"
-        name="email"
-        id="email"
-        title="email"
-        placeholder="E-mail:"
-        autoComplete="off"
-        onChange={(e) => {
-          handleData(e)
-        }}
-      />
-      <input
-        className="myInput"
-        type="password"
-        name="password"
-        id="password"
-        title="password"
-        placeholder="Password:"
-        autoComplete="off"
-        onChange={(e) => {
-          handleData(e)
-        }}
-      />
-      <div className="bottomSection">
-        <div
-          className="designLoginButton"
-          onClick={() => {
-            login(user)
-          }}
-        >
-          Login
-        </div>
-      </div>
-      <div>{error === "no error" ? null : error}</div>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">Login</button>
+    </form>
   )
 }
 
-export default Login*/
+export default Login

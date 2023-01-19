066a433894968e68bff9ab2f37675b1b25d5a7a7
Vanessa Ferreira <vanessa.ferreira.dev@gmail.com>
2023-01-19 10:42:37 +0000

    Register working (still needs some fixes)

--- src/services/validate.js
+++ src/services/validate.js
@@ -1,43 +1,29 @@
-export const errorValidate = (name, value) => {
-  switch (name) {
-    case "name":
-      if ("") {
-        return "Invalid name format"
-      } else {
-        return ""
-      }
+const uppercaseRegExp = /(?=.*?[A-Z])/
+const lowercaseRegExp = /(?=.*?[a-z])/
+const digitsRegExp = /(?=.*?[0-9])/
+const minLengthRegExp = /.{6,}/
+const emailRegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
-    case "email":
-      if (
-        !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
-          value
-        )
-      ) {
-        return "Invalid e-mail format"
-      } else {
-        return ""
-      }
-
-    case "phone":
-      if (!/(?=.*?[0-9])/.test(value)) {
-        return "Phone number incorrect"
-      } else {
-        return ""
-      }
-
-    case "password":
-    case "password2":
-      if (value.length < 6) {
-        return "Write at least 6 characters"
-      } else {
-        if (!/[\d()+-]/g.test(value)) {
-          return "Invalid password format"
-        } else {
-          return ""
-        }
-      }
-
-    default:
-      console.log("Check that all the answers are correct")
+export const validateForm = (user) => {
+  let validationError
+  if (
+    user.password.length === 0 ||
+    user.name.length === 0 ||
+    user.email.length === 0
+  ) {
+    validationError = "Every field must to be filled"
+  } else if (!emailRegExp.test(user.email)) {
+    validationError = "Please insert a valid email format (example@email.com)"
+  } else if (!uppercaseRegExp.test(user.password)) {
+    validationError = "At least one Uppercase"
+  } else if (!lowercaseRegExp.test(user.password)) {
+    validationError = "At least one Lowercase"
+  } else if (!digitsRegExp.test(user.password)) {
+    validationError = "At least one digit"
+  } else if (!minLengthRegExp.test(user.password)) {
+    validationError = "Password needs at least 6 characters"
+  } else {
+    validationError = "no error"
   }
+  return validationError
 }

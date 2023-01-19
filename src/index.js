13ee2a207fdca5be8e4c754b4118c37429153397
Vanessa Ferreira <vanessa.ferreira.dev@gmail.com>
2023-01-19 09:50:01 +0000

    reportWebVitals fixed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react"
-import { reportWebVitals } from "react"
+import reportWebVitals from "./reportWebVitals"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 import { BrowserRouter } from "react-router-dom"
+import { AuthProvider } from "./containers/providers/AuthProvider"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
@@ -17,4 +20,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+
+reportWebVitals(console.log)

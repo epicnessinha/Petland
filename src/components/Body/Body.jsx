77f2acf030ab01674918aa7ad2f2b15c99a700c6
Vanessa Ferreira <vanessa.ferreira.dev@gmail.com>
2023-01-20 09:23:36 +0000

    Design tests and drafts

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -6,17 +6,20 @@ import Register from "../../containers/Register/Register"
 import Profile from "../../containers/Profile/Profile"
 import PetDetails from "../../containers/PetDetails/PetDetails"
 import Admin from "../../containers/Admin/Admin"
+import "./Body.css"
 
 const Body = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/petdetails" element={<PetDetails />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/petdetails" element={<PetDetails />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </>
   )
 }
 
